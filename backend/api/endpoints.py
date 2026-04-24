60730d76b58d0a4e923319b6c93b6ff445727b9e
Vedant005 <vedantkanekar909@gmail.com>
2026-04-24 15:54:13 +0530

    feat: update upload functionality to use unique hex for namespace instead of user_id

--- backend/api/endpoints.py
+++ backend/api/endpoints.py
@@ -1,6 +1,7 @@
-from fastapi import APIRouter, File, UploadFile, HTTPException, Query
+from fastapi import APIRouter, File, UploadFile, HTTPException
 import json
 import os
+import uuid
 from services.processor import DocumentProcessor
 
 router = APIRouter()
@@ -8,8 +9,7 @@ processor = DocumentProcessor()
 
 @router.post("/upload")
 async def upload_pdf(
-    file: UploadFile = File(...), 
-    user_id: str = Query(...)  # Accept user_id from frontend localStorage
+    file: UploadFile = File(...)
 ):
     if file.content_type != "application/pdf":
         raise HTTPException(status_code=400, detail="Only PDF files are allowed")
@@ -19,10 +19,13 @@ async def upload_pdf(
         raw_text = processor.extract_text_from_memory(pdf_bytes)
         chunks = processor.create_chunks(raw_text)
 
-        # Create a unique namespace combining User + Filename
+       
+        unique_hex = uuid.uuid4().hex
+        
+        # Create a unique namespace combining Hex + Filename
         # We strip spaces and dots to keep the namespace clean
         clean_filename = file.filename.replace(" ", "_")
-        unique_namespace = f"{user_id}_{clean_filename}"
+        unique_namespace = f"{unique_hex}_{clean_filename}"
 
         await processor.save_to_pinecone(
             chunks=chunks, 
@@ -32,7 +35,7 @@ async def upload_pdf(
 
         return {
             "filename": file.filename,
-            "namespace": unique_namespace,  # Send this back to frontend for the URL
+            "namespace": unique_namespace,  
             "status": "Processed & Indexed",
             "storage": "Pinecone Cloud"
         }
@@ -42,12 +45,12 @@ async def upload_pdf(
 @router.get("/analyze/{namespace}")
 async def get_analysis(namespace: str):
     """
-    Namespace is now the unique string: {user_id}_{filename}
+    Namespace is now the unique string: {unique_hex}_{filename}
     """
     try:
         raw_analysis = await processor.analyze_contract(namespace)
         
-        # Robust JSON cleaning
+        # JSON cleaning
         json_data = raw_analysis.replace("```json", "").replace("```", "").strip()
         analysis_results = json.loads(json_data)
         
